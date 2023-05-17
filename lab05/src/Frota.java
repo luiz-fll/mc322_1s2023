328ff581360e4ce153661663b6315326d6a310f3
Luiz Fernando Lima Leite <l248405@dac.unicamp.br>
2023-05-16 22:23:14 -0300

    Cria Frota

---
+++ lab05/src/Frota.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Frota {
+    private String code;
+    private ArrayList<Veiculo> listaVeiculos = new ArrayList<Veiculo>();
+
+
+    public Frota(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public ArrayList<Veiculo> getListaVeiculos() {
+        return this.listaVeiculos;
+    }
+
+    public void setListaVeiculos(ArrayList<Veiculo> listaVeiculos) {
+        this.listaVeiculos = listaVeiculos;
+    }
+
+}
