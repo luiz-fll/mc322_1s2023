bee88a26a8a3e6756bbb435c654db181c6ad9628
Luiz Fernando Lima Leite <l248405@dac.unicamp.br>
2023-05-26 22:15:35 -0300

    Muitas coisas, muitas

--- lab05/src/Main.java
+++ lab05/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import javax.naming.NameAlreadyBoundException;
@@ -12,12 +13,49 @@ public class Main {
 
 	public static void menu() {
 		Scanner sc = new Scanner(System.in);
-		Seguradora[] seguradoras = {null, null, null};
-		Menu menuPrincipal = new Menu("Programa Seguradora");
+		ArrayList<Seguradora> seguradoras = new ArrayList<Seguradora>();
+		Seguradora seguradoraSelecionada = null;
+		Cliente clienteSelecionado = null;
+		Seguro seguroSelecionado = null;
+		Condutor condutorSelecionado = null;
+		Menu menuAtual = null;
+		Operacao operacaoAtual = Operacao.MENU_PRINCIPAL;
 
-		for (Seguradora seguradora : seguradoras) {
-			
+		while (true) {
+			switch (operacaoAtual) {
+				case MENU_PRINCIPAL:
+					menuAtual = Menu.MenuPrincipal(seguradoras);
+					menuAtual.mostrar();
+					operacaoAtual = menuAtual.selecionarOperacao(sc);
+					break;
+				case CRIAR_SEGURADORA:
+					System.out.println("Insira os dados da Seguradora...");
+					try {
+						String cnpj = Leitura.lerCNPJ(sc);
+						String nome = Leitura.lerString(sc, "Nome");
+						String telefone = Leitura.lerTelefone(sc);
+						String endereco = Leitura.lerString(sc, "Endereço");
+						String email = Leitura.lerEmail(sc);
+						seguradoraSelecionada = new Seguradora(cnpj, nome, telefone, email, endereco);
+						seguradoras.add(seguradoraSelecionada);
+						operacaoAtual = Operacao.PAINEL_SEGURADORA;
+					} catch (Exception e) {
+						System.out.println(e);
+						operacaoAtual = Operacao.MENU_PRINCIPAL;
+					}
+					break;
+				case PAINEL_SEGURADORA:
+					menuAtual = Menu.PainelSeguradora(seguradoraSelecionada);
+					menuAtual.mostrar();
+					operacaoAtual = menuAtual.selecionarOperacao(sc);
+					break;
+				case PAINEL_SEGURO:
+				// Listar seguros e selecionar um
+					menuAtual = Menu.PainelSeguro(seguroSelecionado);
+					break;
+				default:
+					break;
+			}
 		}
-
 	}
 }
